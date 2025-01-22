8381e99befeed1c9c357613e406649b8e3336714
Kirscher <tristan.kirscher@gmail.com>
2025-01-22 14:32:31 +0100

    [+] add contraction Trainer

---
+++ nnUNet/training/nnUNetTrainer_1000epochs_NoMirroring_contraction.py
@@ -0,0 +1,57 @@
+import torch
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from batchgeneratorsv2.transforms.spatial.spatial import SpatialTransform
+from batchgeneratorsv2.helpers.scalar_type import RandomScalar
+from batchgeneratorsv2.transforms.base.basic_transform import BasicTransform
+from typing import Union, Tuple, List
+import numpy as np
+
+class nnUNetTrainer_1000epochs_NoMirroring_contraction_07to25(nnUNetTrainer):
+    def __init__(
+        self,
+        plans: dict,
+        configuration: str,
+        fold: int,
+        dataset_json: dict,
+        unpack_dataset: bool = True,
+        device: torch.device = torch.device("cuda"),
+    ):
+        super().__init__(
+            plans,
+            configuration,
+            fold,
+            dataset_json,
+            unpack_dataset=unpack_dataset,
+            device=device,
+        )
+        self.num_epochs = 1000 # nnUNet default is 1000
+
+    # here we remove Mirroring from the training transforms
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
+            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
+
+    def get_training_transforms(
+            self,
+            patch_size: Union[np.ndarray, Tuple[int]],
+            rotation_for_DA: RandomScalar,
+            deep_supervision_scales: Union[List, Tuple, None],
+            mirror_axes: Tuple[int, ...],
+            do_dummy_2d_data_aug: bool,
+            use_mask_for_norm: List[bool] = None,
+            is_cascaded: bool = False,
+            foreground_labels: Union[Tuple[int, ...], List[int]] = None,
+            regions: List[Union[List[int], Tuple[int, ...], int]] = None,
+            ignore_label: int = None,
+    ) -> BasicTransform:
+        tr_transforms = super().get_training_transforms(
+            patch_size, rotation_for_DA, deep_supervision_scales, mirror_axes, do_dummy_2d_data_aug,
+            use_mask_for_norm, is_cascaded, foreground_labels, regions, ignore_label
+        )
+        for transform in tr_transforms.transforms:
+            if isinstance(transform, SpatialTransform):
+                transform.scaling = (0.7, 2.5) # defaut is (0.7, 1.4), here we change it to (0.7, 2.5) to allow more contraction
+        return tr_transforms
